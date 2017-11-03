e9513cc27e1deb3041207287bb186775b6f509eb
James Chen <Family@airbears2-10-142-28-57.airbears2.1918.berkeley.edu>
2017-11-02 21:53:51 -0700

    fixing css bugs

--- js/main.js
+++ js/main.js
@@ -1,8 +1,15 @@
-$("#inputbox").keyup(function(event) {
-	if ((event.keyCode || event.which) == 32) {
-		$("#wordbox1").html("hello");
+var $wordbox1 = $("<div>", {"id": "wordbox1", "class": "wordbox", "style": "top: 100px"});
+var $wordbox2 = $("<div>", {"id": "wordbox2", "class": "wordbox", "style": "top: 200px"});
+var emptySearch = true;
+
+$("#input_box").keyup(function(event) {
+	if (emptySearch == true) {
+		$("#search_elements").append($wordbox1)
+		emptySearch = false
 	}
-	else {
-		$("#wordbox1").html($("#inputbox").val());
+	else if ((event.keyCode || event.which) == 32) {
+		$("#search_elements").append($wordbox2);
+		$("#wordbox1").html("hello")
 	}
+	$("#wordbox1").html($("#inputbox").val());
 });
