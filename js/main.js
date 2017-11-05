756cfa4aa1a51bc77007a4dfe5853a736e5af3a5
James Chen <Family@airbears2-10-142-29-22.airbears2.1918.berkeley.edu>
2017-11-04 18:11:23 -0700

    "added flexible input box implementation"

--- js/main.js
+++ js/main.js
@@ -1,15 +1,21 @@
-var $wordbox1 = $("<div>", {"id": "wordbox1", "class": "wordbox", "style": "top: 100px"});
-var $wordbox2 = $("<div>", {"id": "wordbox2", "class": "wordbox", "style": "top: 200px"});
 var emptySearch = true;
+var currentBoxId;
+var nextBoxIndex = 0;
+var inputBoxes = [...Array(35).keys()].map(i => "word_box" + i.toString());
 
 $("#input_box").keyup(function(event) {
 	if (emptySearch == true) {
-		$("#search_elements").append($wordbox1)
-		emptySearch = false
+		currentBoxId = inputBoxes[nextBoxIndex];
+		nextBoxIndex += 1
+		$("#search_elements").append($("<div>", {"id": currentBoxId, "class": "word_box"}));
+		$("#" + currentBoxId).css("top", "25%");
+		emptySearch = false;
 	}
 	else if ((event.keyCode || event.which) == 32) {
-		$("#search_elements").append($wordbox2);
-		$("#wordbox1").html("hello")
+		currentBoxId = inputBoxes[nextBoxIndex];
+		nextBoxIndex += 1
+		$("#search_elements").append($("<div>", {"id": currentBoxId, "class": "word_box"}));
+		$("#" + currentBoxId).css({"top": "35%", "left": "35%"});
 	}
-	$("#wordbox1").html($("#inputbox").val());
+	$("#" + currentBoxId).html($("#input_box").val());
 });
