889f16f6b8b0a56b6330e3eb2365787b1a86905b
James Chen <Family@Yus-MacBook-Pro.local>
2017-11-06 15:51:55 -0800

    fixed bug on input with extra spaces

--- js/main.js
+++ js/main.js
@@ -1,12 +1,49 @@
 var emptySearch = true;
 var currentBoxId;
 var nextBoxIndex = 0;
-var inputBoxes = [...Array(35).keys()].map(i => "word_box" + i.toString());
+var maxSearchElements = 16;
+var wordBoxes = [...Array(maxSearchElements).keys()].map(i => "word_box" + i.toString());
+var boxesOnScreen = [];
 var inputWords;
+
+for (wordBoxIndex = 0; wordBoxIndex < wordBoxes.length; wordBoxIndex++) {
+	currentBoxId = wordBoxes[wordBoxIndex];
+	$("#search_elements").append($("<div>", {"id": currentBoxId, "class": "word_box"}));
+	$("#" + currentBoxId).css("display", "none");
+	$("#" + currentBoxId).css("top", (wordBoxIndex * 50 + 100).toString() + "px");
+	$("#" + currentBoxId).css("left", "50%");
+}
 
 $("#input_box").keyup(function(event) {
 	inputWords = $("#input_box").val().split(" ");
-	if (emptySearch == true) {
+	inputWords = inputWords.filter(function(inputItem){
+		return inputItem !== " " && inputItem !== ""
+	})
+	if (boxesOnScreen.length > inputWords.length) {
+		for (removeBoxesIndex = inputWords.length; removeBoxesIndex < boxesOnScreen.length; removeBoxesIndex++) {
+			$("#" + boxesOnScreen[removeBoxesIndex]).css("display", "none")
+		}
+		boxesOnScreen = boxesOnScreen.slice(0, inputWords.length)
+	}
+	
+	for (inputIndex = 0; inputIndex < inputWords.length; inputIndex++) {
+		if (inputIndex >= boxesOnScreen.length) {
+			$("#" + wordBoxes[inputIndex]).css("display", "block");
+			boxesOnScreen.push(wordBoxes[inputIndex])
+		}
+		$("#" + boxesOnScreen[inputIndex]).html(inputWords[inputIndex]);
+	}
+
+	/* for (i = 0; i < wordBoxes.length; i++) {
+		while (i < inputWords.length) {
+			var currentBox = $("#word_box" + i.toString())
+			if (.length) {
+				$("")
+			}
+		}
+	}
+	
+	inputWords = $("#input_box").val().split(" ");
 		currentBoxId = inputBoxes[nextBoxIndex];
 		nextBoxIndex += 1
 		$("#search_elements").append($("<div>", {"id": currentBoxId, "class": "word_box"}));
@@ -19,5 +56,5 @@ $("#input_box").keyup(function(event) {
 		$("#search_elements").append($("<div>", {"id": currentBoxId, "class": "word_box"}));
 		$("#" + currentBoxId).css({"top": "35%", "left": "35%"});
 	}
-	$("#" + currentBoxId).html(inputWords[inputWords.length - 1]);
+	$("#" + currentBoxId).html(inputWords[inputWords.length - 1]); */
 });
